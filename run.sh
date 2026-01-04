0300ca9dc6b72991d5aa680198080628a9370776
akakaplin <aleksei.kaplin.2021+github2@gmail.com>
2026-01-05 00:51:19 +0100

    Cleanup

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run Ethereum Wallet CLI with Etherscan API key
-
-echo "🚀 Launching Ethereum Wallet CLI..."
-
-# Run with cargo and set Etherscan API key
-ETHERSCAN_API_KEY=2XBIT5RJ9ZTF1Z4W3VK2D63KDYXQUTRU4D cargo run -- "$@"
